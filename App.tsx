19321ea586a6c07ff358b035ca872e155a7df835
Pedro Villarreal <pvillarreal@makingsense.com>
2022-08-25 14:30:36 +0200

    - Fix babel config

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
+
 import { MainScreen } from "_modules/home/screens/MainScreen";
 
 const App = () => {
